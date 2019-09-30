1ea07eea830c6e85dd353db407bdc0808fde633c
Ankita2901 <53752914+Ankita2901@users.noreply.github.com>
2019-09-30 10:42:37 +0530

    Update ContactData.java

--- Final Assessement/src/form/div/collections/ContactData.java
+++ Final Assessement/src/form/div/collections/ContactData.java
@@ -1,87 +1,133 @@
-package form.div.collections;
+package com.dev.Contacts;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
 
-public class ContactData {
-	
-	
+
+
+
+
+public class Dataset {
+
 	public static void main(String[] args) {
-		HashMap<String, Contact> hm = new HashMap<String,Contact>();
-		
-		Contact c1 = new Contact();
-		c1.setName("Ankita");
-		c1.setNumber(26568797l);
-		c1.setGroup("spartans");
-		
-		Contact c2 = new Contact();
-		c2.setName("Ankita");
-		c2.setNumber(26568797l);
-		c2.setGroup("spartans");
-		
-		Contact c3 = new Contact();
-		c3.setName("Ankita");
-		c3.setNumber(26568797l);
-		c3.setGroup("spartans");
-		
-		Contact c4 = new Contact();
-		c4.setName("Ankita");
-		c4.setNumber(26568797l);
-		c4.setGroup("spartans");
-		
-		
-		 Contact d = hm.get(c3);
-		System.out.println("Press-1 to show all contacts" );
-		System.out.println("Press-2 to search for contact Using name");
-		System.out.println("Press-3 to operate on contact");
+		ArrayList<Contact> hm = new ArrayList<Contact>();
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
 
-		int press = 1;
-		if(press==1) {
-			
-			System.out.println(" all the contact names" +hm.get(c1));
-		
-		 if(press==2) {
-			 
-			 boolean j = hm.containsKey(c3);
-				System.out.println("complete information" );
-				System.out.println("press 1 to call");
-				System.out.println("press 2 to message");
-				System.out.println("press 3 to go back to main menu");
-				int press1=3;
-				if(press1==1) {
-					System.out.println("calling to:"+c1.getName());
-					if(press1==2) {
-						System.out.println("Sending message");
-					}
-						if(press1==3) {
-							System.out.println("go back to the main menu:");
-								
-						}
-						if(press==3) {
-							System.out.println("Press 1 to add contact");
-							System.out.println("Press 2 to delete contact");
-							System.out.println("Press 3 to edit contact"); 
-						}
-					}
+		Contactlist b = new Contactlist();
+		Contact a = new Contact();
+		Contact a1 = new Contact();
+
+		System.out.println("WELCOME TO CONTACT APPLICATION");
+		int op;
+		do {
+			System.out.println("enter 1 to Addcontact and show all contacts // enter 2 to search // enter 3 operate on contact");
+			int i = sc.nextInt();
+			switch(i)
+			{
+			case 1 :
+				System.out.println("enter name");
+				String d = sc.next();
+				a.setName(d);
+				System.out.println("enter phone no");
+				long j = sc.nextLong();
+				a.setPhoneNo(j);
+				System.out.println("enter group type");
+				String l = sc.next();
+				a.setGroup(l);
+				hm.add(a);
+				System.out.println("*************************************************");
+				System.out.println("account is created and contact is added");
+				System.out.println("if u wish to create another account then press 1 ");
+				int w = sc.nextInt();
+				if(w==1)
+				{
 					
+					System.out.println("enter name");
+					String d1 = sc.next();
+					a1.setName(d1);
+					if(a.getName().equals(a1.getName()))
+					{
+						System.out.println("name is already in contact");
+
+					}
+					else {
+						System.out.println("enter phone no");
+						long j1 = sc.nextLong();
+						a1.setPhoneNo(j1);
+						System.out.println("enter group type");
+						String l1 = sc.next();
+						a1.setGroup(l1);
+						hm.add(a1);
+					}
+
+				}
+				System.out.println("*********************************************");
+
+
+				HashSet<Contact> arr = new HashSet<>(hm);
+
+				System.out.println(arr);
+				System.out.println("contact name is displayed");
+				System.out.println(a.getName());
+				System.out.println(a1.getName());
+
+				break;
+			case 2 :
+				System.out.println("enter the name");
+				String n = sc.next();
+				if(n.equals(a.getName()))
+				{
+					System.out.println(a);
+					b.contact(a);
+
+
+
+				}
+				else if(n.equals(a1.getName()))
+				{
+					System.out.println(a1);
+					b.contact(a1);
+				}
+				else
+					System.out.println("name not in contact");
+				break;
+			case 3 :
+				System.out.println("wellcome to CURD Operations");
+				System.out.println("1 to delete 2 to edit");
+				int s2=sc.nextInt();
+				if(s2==1)
+				{
+					System.out.println(a1);
+					System.out.println(hm.remove(a1));	
+					System.out.println(a1);
+
+				}
+				else if(s2==2)
+				{
+					System.out.println(a);
+					b.editContact(a);
 					
 				}
-					
-					
-				}
-				
-			
-			
-		
-		
-		
+				else
+					System.out.println("invalid");
+				break;
 
+
+
+			default :
+				System.out.println("invalid input");
+
+
+			}
+			System.out.println("to continue press 1 // press 0 to exit");
+			op = sc.nextInt();
+
+		}while(op==1);
+		System.out.println("thanks");
 	}
 
 
-
-
-		
-		}
-
-	
+}
 
