97409982ebbd6272f3aa5a8d82a228a7f7e88a52
Ankita2901 <53752914+Ankita2901@users.noreply.github.com>
2019-09-30 10:50:52 +0530

    Update ContactImplements.java

--- Final Assessement/src/form/div/collections/ContactImplements.java
+++ Final Assessement/src/form/div/collections/ContactImplements.java
@@ -15,23 +15,23 @@ public class ContactImp {
        int i = sc.nextInt();
        if(i==1)
        {
-    	   System.out.println("enter the number to call");
+    	   System.out.println("enter the number");
     	    long l = sc.nextLong();
-    	    System.out.println("calling............"+l);
+    	    System.out.println("calling"+l);
     	    
-    	    System.out.println("call ended !!!");
+    	    System.out.println("call terminated");
        }
        else if(i==2)
        {
     	   System.out.println("enter the message");
    	    String s = sc.next();
    	    value.setMessage(s);
-   	    System.out.println("message is sending..............");
+   	    System.out.println("message is sending");
     	   
        }
        else
        {
-    	   System.out.println("invalid.......");
+    	   System.out.println("invalid");
        }
        System.out.println("enter 1 to continue 2 go out");
         w = sc.nextInt();
@@ -43,7 +43,7 @@ public class ContactImp {
 		System.out.println("enter name");
 		String s3= sc.next();
 		value.setName(s3);
-		System.out.println("enter phoneNo");
+		System.out.println("enter phone number");
 		long s4= sc.nextLong();
 		value.setPhoneNo(s4);
 		
